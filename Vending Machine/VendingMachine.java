7f341893b5599128a09c50fa1785185836a4c917
fczheng88 <fczheng88@gmail.com>
2015-09-22 13:44:20 -0500

    Completed VendingMachine

---
+++ Vending Machine/VendingMachine.java
@@ -0,0 +1,66 @@
+
+/**
+ * Vending Machine.
+ * 
+ * @author Felix Zheng
+ * @version 1
+ */
+public class VendingMachine
+{
+    // instance variables - replace the example below with your own
+    private int cans;
+    private int tokens;
+    
+    public VendingMachine()
+    {
+        this.cans = 10;
+        this.tokens = 0;
+    }
+    /**
+     * Constructor for objects of class VendingMachine
+     */
+    public VendingMachine(int cans)
+    {
+        // initialise instance variables
+        this.cans = cans;
+        this.tokens = 0;
+    }
+    
+
+    /**
+     * Method to buy a can of soda
+     * 
+     * @pre there are cans in the vending machines
+     */
+    public void vend()
+    {
+        this.tokens += 1;
+        this.cans -= 1;
+    }
+    /**
+     * Refill the vending machine and remove all tokens
+     * 
+     * @param int cans - the number of cans to add
+     * @post tokens - set to 0 because they were removed 
+     */
+    public void refill(int cans)
+    {
+        this.cans += cans;
+        this.tokens = 0;
+    }
+    /**
+     * Gets the number of tokens in the vending machine
+     */
+    public int getTokens()
+    {
+        return this.tokens;
+    }
+    /**
+     * Gets the number of cans in the vending machine
+     * 
+     */
+    public int getCans()
+    {
+        return this.cans;
+    }
+}
---
+++ Vending Machine/VendingMachineTest.java
@@ -0,0 +1,68 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class VendingMachineTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class VendingMachineTest
+{
+    /**
+     * Default constructor for test class VendingMachineTest
+     */
+    public VendingMachineTest()
+    {
+    }
+    @Test
+    public void testInit()
+    {
+        VendingMachine thing = new VendingMachine();
+        assertEquals(10, thing.getCans());
+    }
+    @Test
+    public void testFillUp()
+    {
+        VendingMachine thing = new VendingMachine();
+        thing.refill(15);
+        assertEquals(25, thing.getCans());
+        thing.refill(30);
+        assertEquals(55, thing.getCans());
+        thing.vend();
+        assertEquals(1, thing.getTokens());
+    }
+    @Test
+    public void testVend()
+    {
+        VendingMachine thing = new VendingMachine();
+        thing.vend();
+        assertEquals(9, thing.getCans());
+        assertEquals(1, thing.getTokens());
+    }
+    @Test 
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
